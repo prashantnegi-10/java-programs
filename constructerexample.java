34202dfd7709a8f050ecabda8fbd8b5570820b04
prashantnegi-10 <115805615+prashantnegi-10@users.noreply.github.com>
2023-03-22 15:53:03 +0530

    Create constructerexample.java

---
+++ constructerexample.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Test
+{
+    Test()
+    {
+        System.out.println("hello i am test constructer");
+    }
+}
+class Solution
+{
+    public static void main(String[] args) 
+     {
+         Test t = new Test();
+     }
+}
