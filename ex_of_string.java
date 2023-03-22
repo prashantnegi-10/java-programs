b7937d1f569364e3c92c63e21db8a4e457164900
prashantnegi-10 <115805615+prashantnegi-10@users.noreply.github.com>
2023-03-22 16:12:47 +0530

    Create ex_of_string.java

---
+++ ex_of_string.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String[] agrs)
+    {
+        String s="Hello";
+        System.out.println(s);
+        s.concat("my friend");
+        System.out.println(s);
+        s=s.concat("to all");
+        System.out.println(s);
+        
+    }
+}
